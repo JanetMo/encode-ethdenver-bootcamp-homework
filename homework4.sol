ec28c2886f6134eb153cdd660534edd160461bf2
JanetMo <m.marz@kabelmail.de>
2022-11-11 19:45:29 +0100

    adapt

--- homework4.sol
+++ homework4.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 contract VolcanoCoin {
     // Create a variable to hold the total supply of 10000
@@ -13,15 +13,16 @@ contract VolcanoCoin {
     address private owner;
     
     // In order to keep track of user balances, we need to associate a user's address with the balance that they have
-    // Using your choice of data structure, set up a variable called balances to keep track of the number of volcano coins that a user has
+    // Using your choice of data structure, set up a variable called balances 
+    // to keep track of the number of volcano coins that a user has
     mapping(address => uint256) private balances;
 
     // We want to have a payments array for each user sending the payment
     // Create a mapping which returns an array of Payment structs when given this user's address
     mapping(address => Payment[]) private transfers;
 
-    event Mint(uint256 newTotalSupply);
-    event Transfer(uint256 amount, address indexed recipient);
+    event TotalSupplyIncreased(uint256 newTotalSupply);
+    event TransferEvent(uint256 amount, address indexed recipient);
 
     // We want to keep a record for each user's transfers
     // Create a struct called Payment that stores the transfer amount and the recipient's address
@@ -32,11 +33,12 @@ contract VolcanoCoin {
 
     // Next, create a modifier which only allows an owner to execute certain functions
     modifier onlyOwner() {
-            require(msg.sender == owner, "This is an owner only function");
-            _;
-        }
+        require(msg.sender == owner, "This is an owner only function");
+        _;
+    }
     // The contract owner's address should only be updateable in one place
     // Create a constructor and within the constructor, store the owner's address
+
     constructor() {
         owner = msg.sender;
         balances[owner] = totalSupply;
@@ -56,32 +58,32 @@ contract VolcanoCoin {
     // change the constructor, to give all of the total supply to the owner of the contract
     function increaseTotalSupply() public onlyOwner {
         totalSupply += 1000;
-        balances[owner] += 1000;
-        emit Mint(totalSupply);
+        // balances[owner] += 1000;
+        emit TotalSupplyIncreased(totalSupply);
     }
 
     // We want to allow the balances variable to be read from the contract
-    function getAccountBalance(address _account) public view returns (uint256){
-        require(msg.sender == _account, "You are not the holder of this address");
-        return balances[_account];
+    function getAccountBalance(address account) public view returns (uint256){
+        require(msg.sender == account, "You are not the holder of this address");
+        return balances[account];
     }
 
-    function getAccountTransfers(address _account) public view returns (Payment[] memory){
-        require(msg.sender == _account, "Stop counting someone else's pockets");
-        return transfers[_account];
+    function getAccountTransfers(address account) public view returns (Payment[] memory){
+        require(msg.sender == account, "Stop counting someone else's pockets");
+        return transfers[account];
     }
 
     //  Now add a public function called transfer to allow a user to transfer their tokens to another address
     //  This function should have 2 parameters : the amount to transfer and the recipient address.
 
-    function transfer(uint256 _amount, address _recipient) public {
-        require(_amount <= balances[msg.sender], "You can't transfer more tokens than you have");
-        balances[msg.sender] -= _amount;
-        balances[_recipient] += _amount;
-        transfers[msg.sender].push(Payment(_amount, _recipient));
+    function transfer(uint256 amount, address recipient) public {
+        require(amount <= balances[msg.sender], "You can't transfer more tokens than you have");
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+        transfers[msg.sender].push(Payment(amount, recipient));
         // Add an event to the transfer function to indicate that a transfer has taken place
         // It should log the amount and the recipient address
-        emit Transfer(_amount, _recipient);
+        emit TransferEvent (amount, recipient);
     }
 
 }
